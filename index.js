96a762ed2804bbbf232f1d82d4736857e2345f18
Amberhva <amber.van.der.reijden@hva.nl>
2023-02-23 14:40:36 +0100

    Update
    Added anchors to navigate through the different squads, made a few lay-out changes so the cards are always centered, and changed the media-queries so that you keep seeing less people on one row.

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 import express from "express";
 
+// Squad A
+const urlA = "https://whois.fdnd.nl/api/v1/squad/squad-a-2022";
+const dataA = await fetch(urlA)
+    .then((response) => response.json())
+    .catch((error) => error);
+
+console.log(dataA);
+
+// Squad B
+const urlB = "https://whois.fdnd.nl/api/v1/squad/squad-b-2022";
+const dataB = await fetch(urlB)
+    .then((response) => response.json())
+    .catch((error) => error);
+
+console.log(dataB);
+
+// Squad C
 const url = "https://whois.fdnd.nl/api/v1/squad/squat-c-2022";
 const data = await fetch(url)
     .then((response) => response.json())
@@ -15,16 +32,26 @@ app.set("view engine", "ejs");
 app.set("views", "./views");
 app.use(express.static("public"));
 
-// Maak een route voor de index
+// Maak een route voor de index voor squad A
+app.get("/squadA", (request, response) => {
+    console.log(request.query.squad);
+
+    response.render("index", dataA);
+});
+
+// Maak een route voor de index voor squad B
+app.get("/squadB", (request, response) => {
+    console.log(request.query.squad);
+
+    response.render("index", dataB);
+});
+
+// Maak een route voor de index voor squad C
 app.get("/", (request, response) => {
     console.log(request.query.squad);
 
     response.render("index", data);
 });
-
-// app.get('/members', (request, response) => {
-//   response.send('Joepie!!')
-// })
 
 // Stel het poortnummer in en start express
 app.set("port", process.env.PORT || 8000);
